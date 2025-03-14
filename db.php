4cb8991af02e530af24d32cf85fc9789e36180ec
holyscheidt <160267102+holyscheidt@users.noreply.github.com>
2025-03-14 18:57:51 -0500

    Update db.php codesprint 3

--- db.php
+++ db.php
@@ -2,13 +2,9 @@
     $servername = "localhost";
     $username = "root"; // Default XAMPP username
     $password = ""; 
-    $database = "498db";
+    $database = "csv_db 8";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
 
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
 ?>
