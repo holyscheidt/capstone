fb32821c5bd026f05d9701da77fcaa3985d849f2
holyscheidt <160267102+holyscheidt@users.noreply.github.com>
2025-03-24 11:34:54 -0500

    Update db.php code sprint 4

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root"; // Default XAMPP username
     $password = ""; 
-    $database = "csv_db 8";
+    $database = "csv_db 8"; //db name
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
